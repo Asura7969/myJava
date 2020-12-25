13c5dd1fddf9fa459f311c1c49c15a4c36fb278c
gongwenzhou <gongwenzhou@hellobike.com>
2020-12-25 15:01:35 +0800

    [add]-自定义锁

--- myJava-juc/src/main/java/org/myJava/juc/queue/PrintlnNumber.java
+++ myJava-juc/src/main/java/org/myJava/juc/queue/PrintlnNumber.java
@@ -1,6 +1,9 @@
 package org.myJava.juc.queue;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * 多线程交替打印自然数
@@ -8,6 +11,81 @@ import java.util.concurrent.TimeUnit;
 public class PrintlnNumber {
 
     public static void main(String[] args) {
+
+//        synchronizedPrintNum();
+        lockPrintNum();
+
+
+
+    }
+
+    static void lockPrintNum(){
+        ReentrantLock lock = new ReentrantLock();
+        Condition condition1 = lock.newCondition();
+        Condition condition2 = lock.newCondition();
+        Condition condition3 = lock.newCondition();
+
+        CountDownLatch c = new CountDownLatch(1);
+
+        double[] data1 = new double[]{1,2.5,4,5.5,7,8.5,10};
+        double[] data2 = new double[]{1.5,3,4.5,6,7.5,9,10.5};
+        double[] data3 = new double[]{2,3.5,5,6.5,8,9.5,11};
+
+        new Thread(() -> {
+            lock.lock();
+            try{
+                for (int i = 0; i < data1.length; i++) {
+                    System.out.println(data1[i]);
+                    condition2.signal();
+                    c.countDown();
+                    condition1.await();
+                }
+                condition1.signalAll();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        },"work1").start();
+
+        new Thread(() -> {
+
+            lock.lock();
+            try{
+                c.await();
+                for (int i = 0; i < data2.length; i++) {
+                    System.out.println(data2[i]);
+                    condition3.signal();
+                    condition2.await();
+                }
+                condition2.signalAll();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        },"work2").start();
+
+        new Thread(() -> {
+
+            lock.lock();
+            try{
+                c.await();
+                for (int i = 0; i < data3.length; i++) {
+                    System.out.println(data3[i]);
+                    condition1.signal();
+                    condition3.await();
+                }
+                condition3.signalAll();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        },"work3").start();
+    }
+
+    static void synchronizedPrintNum(){
         Object obj = new Object();
         Thread a = new Thread(new PrintNum(obj, true), "thread-1");
         Thread b = new Thread(new PrintNum(obj, false), "thread-2");
@@ -18,7 +96,6 @@ public class PrintlnNumber {
         c.start();
         d.start();
         a.start();
-
     }
 
 
