ea3c65b6d39fc8171bf8840e7aac4e5e29b2b3d6
asura7969 <1402357969@qq.com>
2021-01-30 13:05:34 +0800

    future测试

--- myJava-juc/src/main/java/org/myJava/juc/future/CompletableFutureTest.java
+++ myJava-juc/src/main/java/org/myJava/juc/future/CompletableFutureTest.java
@@ -7,16 +7,23 @@ import java.util.concurrent.TimeoutException;
 
 public class CompletableFutureTest {
 
-    public static void main(String[] args) throws InterruptedException, ExecutionException, TimeoutException {
-        CompletableFuture<String> future = new CompletableFuture<>();
-        future.complete(syncIo());
-
-        String s = future.get(1000L, TimeUnit.MILLISECONDS);
-        System.out.println(s);
+    public static void main(String[] args) throws InterruptedException {
+        CompletableFuture<String> cf = CompletableFuture.supplyAsync(CompletableFutureTest::syncIo);
+        System.out.println("main 线程等待返回结果！");
+        cf.thenAccept((result) -> {
+            System.out.println("返回结果是: " + result);
+        });
+        // 异常处理
+        cf.exceptionally((e) -> {
+            e.printStackTrace();
+            return null;
+        });
+        Thread.sleep(10000L);
     }
 
     public static String syncIo() {
         try {
+            System.out.println(Thread.currentThread().getName() + " 执行调用！");
             Thread.sleep(2000L);
         } catch (InterruptedException e) {
             e.printStackTrace();
